47e508c96b0f50f3916e7ba1e0ffee20c8ef920a
sanketbansal57@gmail.com <sanketbansal57@gmail.com>
2019-07-18 21:40:00 -0400

    Fixed bugs
    removed duplicate import from app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -75,7 +75,6 @@ import { EditphasemodalComponent } from './components/challenge/challengephases/
 import {
   ChallengeviewallsubmissionsComponent
 } from './components/challenge/challengeviewallsubmissions/challengeviewallsubmissions.component';
-import { SideBarComponent } from './components/utility/side-bar/side-bar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 
